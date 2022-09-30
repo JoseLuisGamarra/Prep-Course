76ecacd2b11cc93959e0e919913a851302962574
JoseLuisGamarra <jolugapa1@gmail.com>
2022-09-30 18:34:13 -0500

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -53,14 +53,18 @@ function multiplica(x, y) {
 function divide(x, y) {
   // Divide "x" entre "y" y devuelve el valor
   // Tu código:
-  
+  return x / y;
 }
 
 function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (x===y){
+    return true;
+  } else {
+    return false;
+  }  
 }
 
 function tienenMismaLongitud(str1, str2) {
